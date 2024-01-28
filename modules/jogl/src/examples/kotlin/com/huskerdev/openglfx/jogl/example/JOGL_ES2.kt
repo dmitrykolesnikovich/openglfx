ed55ff39500cadf9cead5961acc37532eeebae70
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2024-01-29 02:36:25 +0300

    lwjgl2.cap

--- modules/jogl/src/examples/kotlin/com/huskerdev/openglfx/jogl/example/JOGL_ES2.kt
+++ modules/jogl/src/examples/kotlin/com/huskerdev/openglfx/jogl/example/JOGL_ES2.kt
@@ -3,6 +3,7 @@ import javafx.application.Application
 
 fun main(){
     System.setProperty("prism.order", "es2")
+    System.setProperty("prism.verbose", "true")
     System.setProperty("prism.vsync", "false")
 
     Application.launch(ExampleApp::class.java)
---
+++ modules/lwjgl2/.renderdoc/lwjgl2.bat
@@ -0,0 +1,4 @@
+@echo off
+
+cd %~dp0\..
+java -jar D:\workspace\openglfx\modules\lwjgl2\build\libs\ES-4.0.5.jar
